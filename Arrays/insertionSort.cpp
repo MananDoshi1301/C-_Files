9119b0a9d1669845b3e18aefcc3df9da2644e6af
Manan Doshi <MANANDOSHI1301@GMAIL.COM>
2023-01-17 01:08:10 +0530

    Insertion Sort in arrays

--- Arrays/insertionSort.cpp
+++ Arrays/insertionSort.cpp
@@ -1,44 +1,45 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-void outputArr (int arr[], int n){
-    for(int i=0; i<n; i++){
-        cout << arr[i] << " ";
-    }
+void outputArr(int arr[], int n) {
+  for (int i = 0; i < n; i++) {
+    cout << arr[i] << " ";
+  }
 }
 
-int main(){
-    
-    // INPUT
-    int n;
-    cout << "Enter number of elements for sorting: ";
-    cin >> n;
+int main() {
 
-    cout << "Enter " << n << " elements with a space: ";
+  // INPUT
+  int n;
+  cout << "Enter number of elements for sorting: ";
+  cin >> n;
 
-    int arr[n];
-    
-    for ( int i=0; i<n; i++){
-        cin >> arr[i];                
+  cout << "Enter " << n << " elements with a space: ";
+
+  int arr[n];
+
+  for (int i = 0; i < n; i++) {
+    cin >> arr[i];
+  }
+
+  // FUNCTION
+  int j, current;
+  for (int i = 1; i < n; i++) {
+
+    j = i - 1;
+    current = arr[i];
+
+    while (current < arr[j] && j >= 0) {
+      arr[j + 1] = arr[j];
+      j--;
     }
+    arr[j + 1] = current;
+  }
 
-    // FUNCTION
-    int j, current;
-    for (int i=1; i<n; i++){
-        j = i - 1;
-        current = arr[i];
-        while(arr[i] < arr[j] && j >= 0){
-            // swap 
-            arr[j+1] = arr[j];
-            j--;
-        }
-        arr[j+1] = current;
-    }
+  // OUTPUT
+  cout << "Sorted array is as follows:\n";
+  outputArr(arr, n);
 
-    // OUTPUT 
-    cout << "Sorted array is as follows:\n";    
-    outputArr(arr, n);
-    
-    return 0;
+  return 0;
 }
