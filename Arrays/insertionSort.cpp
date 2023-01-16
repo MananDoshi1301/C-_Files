81264b25b5b84e7fff9b756c1ebb627031e1d6bf
Manan Doshi <manandoshi1301@gmail.com>
2023-01-17 00:28:41 +0530

    In Progress

---
+++ Arrays/insertionSort.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+
+using namespace std;
+
+void outputArr (int arr[], int n){
+    for(int i=0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+}
+
+int main(){
+    
+    // INPUT
+    int n;
+    cout << "Enter number of elements for sorting: ";
+    cin >> n;
+
+    cout << "Enter " << n << " elements with a space: ";
+
+    int arr[n];
+    
+    for ( int i=0; i<n; i++){
+        cin >> arr[i];                
+    }
+
+    // FUNCTION
+    int j, current;
+    for (int i=1; i<n; i++){
+        j = i - 1;
+        current = arr[i];
+        while(arr[i] < arr[j] && j >= 0){
+            // swap 
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = current;
+    }
+
+    // OUTPUT 
+    cout << "Sorted array is as follows:\n";    
+    outputArr(arr, n);
+    
+    return 0;
+}
---
+++ Teasers/maxRacesForHorses.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+  int n, maxRaces;
+  cout << "Enter number of horses: ";
+  cin >> n;
+
+  cout << "Enter number of horses max can race at a time: ";
+  cin >> maxRaces;
+
+  
+  return 0;
+}
