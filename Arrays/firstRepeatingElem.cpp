ee01fdca54a9dd0c448f6a91849b047c39645a3d
Manan Doshi <MANANDOSHI1301@GMAIL.COM>
2023-02-04 13:58:13 +0530

    Restructuring of dir

---
+++ Arrays/firstRepeatingElem.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<climit>
+
+using namespace std;
+
+void firstRepeatingElem(int arr[], int n){
+
+
+}
+
+int main(){
+
+  // INPUT
+  int n, max = INT_MIN;
+  cin >> n;
+
+  int arr[n];
+  for (int i = 0; i < n; i++) {
+    cin >> arr[i];
+  }
+
+  // FUNCTION
+  // OUTPUT
+  return 0;
+}
